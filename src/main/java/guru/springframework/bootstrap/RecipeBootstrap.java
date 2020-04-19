e339ae5c204eb336d5e9c5dadb3844e78f8deb6f
bizzysven <bizzysven@gmail.com>
2020-04-19 17:12:44 -0500

    Added reactive repos, added more tests

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -36,6 +36,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
+
         loadCategories();
         loadUom();
         recipeRepository.saveAll(getRecipes());
--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -1,12 +1,11 @@
 package guru.springframework;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
+//@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
@@ -14,5 +13,4 @@ public class Spring5RecipeAppApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
 }
